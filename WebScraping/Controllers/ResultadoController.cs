2817a733f30e0da2e2d73756cdea435b7abdc635
Alan-Reis <69443030+Alan-Reis@users.noreply.github.com>
2021-08-31 07:34:31 -0300

    Adicionar arquivos de projeto.

---
+++ WebScraping/Controllers/ResultadoController.cs
@@ -0,0 +1,37 @@
+﻿using HtmlAgilityPack;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WebScraping.Controllers
+{
+    public class ResultadoController : Controller
+    {
+        public IActionResult Index()
+        {
+            string url = "http://loterias.caixa.gov.br/wps/portal/loterias/landing/lotofacil/!ut/p/a1/04_Sj9CPykssy0xPLMnMz0vMAfGjzOLNDH0MPAzcDbz8vTxNDRy9_Y2NQ13CDA0sTIEKIoEKnN0dPUzMfQwMDEwsjAw8XZw8XMwtfQ0MPM2I02-AAzgaENIfrh-FqsQ9wBmoxN_FydLAGAgNTKEK8DkRrACPGwpyQyMMMj0VAcySpRM!/dl5/d5/L2dBISEvZ0FBIS9nQSEh/pw/Z7_HGK818G0K85260Q5OIRSC42046/res/id=historicoHTML/c=cacheLevelPage/=/";
+            var response = CallUrl(url).Result;
+            ParseHtml(response);
+            return View();
+        }
+        private static async Task<string> CallUrl(string fullUrl)
+        {
+            HttpClient client = new HttpClient();
+            var response = client.GetStringAsync(fullUrl);
+            return await response;
+        }
+
+        private void ParseHtml(string html)
+        {
+            HtmlDocument htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+            htmlDoc.Save("Resultado/LotoFacil.csv");                   
+        }
+       
+    }
+}
---
+++ WebScrapingConsole/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using HtmlAgilityPack;
+
+namespace WebScrapingConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HtmlAgilityPack.HtmlWeb website = new HtmlAgilityPack.HtmlWeb();
+                        
+            HtmlAgilityPack.HtmlDocument document = website.Load("C:\\Users/alanc/source/repos/Loterias/WebScraping/Resultado/LotoFacil.csv");
+            
+            var td = document.DocumentNode.SelectNodes("//td").ToList();
+            //var tr = document.DocumentNode.SelectNodes("//tr").ToList();
+            //var coinData = new Dictionary<string, string>();
+                        
+                int total = 1;
+
+                foreach (var tds in td)
+                {
+
+                
+                //coinData.Add(tds.ChildNodes[2].InnerText, tds.ChildNodes[3].InnerText);
+               
+                total++;
+
+                 if(total <= 34)
+                 {
+                     Console.Write(tds.InnerText);
+                 }
+                 else
+                 {
+                     Console.WriteLine("");
+                     total = 1;
+                 }
+            }
+                
+
+
+
+        }
+        public static void Ler()
+        {
+            String line;
+            try
+            {
+                //Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader("C:\\Users/alanc/source/repos/Loterias/WebScrapingConsole/teste.txt");
+                //Read the first line of text
+                line = sr.ReadLine();
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+                    //write the lie to console window
+                    Console.WriteLine(line);
+                    //Read the next line
+                    line = sr.ReadLine();
+                }
+                //close the file
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+       
+
+        public static void LerLinha()
+        {
+            int counter = 0;
+            string line;
+
+            // Read the file and display it line by line.  
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(@"C:\\Users/alanc/source/repos/Loterias/WebScrapingConsole/resultado.csv");
+            while ((line = file.ReadLine()) != null)
+            {
+                System.Console.WriteLine(line);
+                counter++;
+            }
+
+            file.Close();
+            System.Console.WriteLine("There were {0} lines.", counter);
+            // Suspend the screen.  
+            System.Console.ReadLine();
+        }
+
+        public static void Pesquisar()
+        {
+            String varPalavra = "tr";
+
+      StreamReader re = File.OpenText("C:\\Users/alanc/source/repos/Loterias/WebScrapingConsole/resultado.csv");
+      string input = re.ReadToEnd();
+
+      if (input.IndexOf(varPalavra) > -1)
+        Console.WriteLine("Existe a palavra '" + varPalavra + "' no arquivo txt");
+      else
+        Console.WriteLine("Não existe a palavra '" + varPalavra + "' no arquivo txt");
+
+      re.Close();
+        }
+    }
+}
