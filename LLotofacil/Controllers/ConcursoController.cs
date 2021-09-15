95c7d80bb8af221ced26997029da570e8e5c5e08
Alan-Reis <69443030+Alan-Reis@users.noreply.github.com>
2021-09-14 21:51:17 -0300

    Controller

---
+++ LLotofacil/Controllers/ConcursoController.cs
@@ -0,0 +1,27 @@
+﻿using LLotofacil.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LLotofacil.Controllers
+{
+    public class ConcursoController : Controller
+    {
+        private readonly IConcurso _Concurso;
+        public ConcursoController(IConcurso _IConcurso)
+        {
+            _Concurso = _IConcurso;
+        }
+        
+        public IActionResult Index()
+        {
+            return View(_Concurso.GetConcursos);
+        }
+        public IActionResult Resultados()
+        {
+            return View(_Concurso.GetConcursos);
+        }
+    }
+}
